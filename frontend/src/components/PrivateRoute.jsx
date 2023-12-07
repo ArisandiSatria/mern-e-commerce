1acf68bfe77b184783d17b51c4cea21ad00ff59e
Arisandi <arisandisatria541@gmail.com>
2023-12-07 20:21:01 +0700

    add validation if cookie present

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Outlet, Navigate } from 'react-router-dom'
-import { useRecoilValue } from 'recoil'
+import { useRecoilState, useRecoilValue } from 'recoil'
 import { userIsLoggedIn } from '../state/selector/loggedInUser'
+import Cookies from 'js-cookie';
+import { userState } from '../state/atom/userState';
 
 const PrivateRoute = () => {
-  const user = useRecoilValue(userIsLoggedIn)
+  const userData = useRecoilValue(userIsLoggedIn)
+  const [user, setUser] = useRecoilState(userState);
+
+  const isCookiePresent = Cookies.get('access_token')
 
   return (
-    user ? <Outlet/> : <Navigate to="/login"/>
+    userData && isCookiePresent ? <Outlet /> : (setUser(null), <Navigate to="/login" />)
   )
 }
 
