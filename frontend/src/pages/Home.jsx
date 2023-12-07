80a28bf1b950bd024f9308c37602cdf341a00d0b
Arisandi <arisandisatria541@gmail.com>
2023-12-07 22:23:55 +0700

    add banner and some featured in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import banner from "../assets/banner.jpg";
+import { FaShippingFast, FaOpencart } from "react-icons/fa";
+import { MdOutlinePayment } from "react-icons/md";
+import { BsClockHistory } from "react-icons/bs";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="p-10 px-2 max-w-6xl mx-auto">
+      <img
+        className="center object-contain shadow-lg"
+        src={banner}
+        alt="banner"
+      />
 
-export default Home
+      <div className="flex gap-4 my-5">
+        <div className="w-xl flex gap-2 rounded-lg shadow-md p-3">
+          <FaShippingFast className="text-3xl text-blue-300" />
+          <div>
+            <p className="font-semibold">Free Shipping</p>
+            <p>We offer free shipping on special products</p>
+          </div>
+        </div>
+        <div className="w-xl flex gap-2 rounded-lg shadow-md p-3">
+          <MdOutlinePayment className="text-3xl text-yellow-300" />
+          <div>
+            <p className="font-semibold">Secure Payment</p>
+            <p>Make secure payment for your order</p>
+          </div>
+        </div>
+        <div className="w-xl flex gap-2 rounded-lg shadow-md p-3">
+          <FaOpencart className="text-3xl text-pink-300" />
+          <div>
+            <p className="font-semibold">Quality Products</p>
+            <p>We only sell products from only tested and proven brands</p>
+          </div>
+        </div>
+        <div className="w-xl flex gap-2 rounded-lg shadow-md p-3">
+          <BsClockHistory className="text-3xl text-lime-300" />
+          <div>
+            <p className="font-semibold">24/7 Support</p>
+            <p>Get access to support from our experts support team</p>
+          </div>
+        </div>
+      </div>
+
+      <div></div>
+    </div>
+  );
+};
+
+export default Home;
