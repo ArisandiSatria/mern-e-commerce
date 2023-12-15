7530faf4ef30e65fee9d7eb075770152ded576da
Arisandi <arisandisatria541@gmail.com>
2023-12-15 14:20:07 +0700

    fixed vite.config bug, and add key for category bar in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -82,6 +82,7 @@ const Home = () => {
       <div className="flex justify-center w-fit mx-auto bg-[#FF9376] rounded-lg overflow-hidden">
         {categories.map((cate) => (
           <span
+            key={cate}
             onClick={() => setClicked(cate)}
             className={`text-white text-lg p-2 px-6 cursor-pointer hover:bg-[#e67353] ${
               clicked == cate ? "bg-[#e67353]" : ""
@@ -92,33 +93,39 @@ const Home = () => {
         ))}
       </div>
 
-      <div className="flex px-[31px] flex-wrap gap-6">{
-        filtered.length > 0 ? filtered.map(flt => (
-          <div
-            key={flt._id}
-            className="bg-white cursor-pointer shadow-md hover:shadow-lg overflow-hidden transition-shadow rounded-lg w-[250px]"
-          >
-            <img
-              src={flt.imageUrls[0]}
-              alt="product image"
-              className="h-[320px] sm:h-[180px] w-full object-cover hover:scale-105 transition-scale duration-300"
-            />
-            <div className="p-3 flex flex-col gap-2 w-full">
-              <p className="text-lg font-semibold text-slate-700 truncate">
-                {flt.name}
-              </p>
-              <div className="flex items-center gap-1">
-                <p className="text-sm text-gray-600 truncate w-full">
-                  Rp{" "}
-                  {(
-                    +flt.regularPrice -
-                    +flt.discountPrice
-                  ).toLocaleString("en-US")}
-                </p>
-              </div>
-            </div>
-          </div>)) : <p className="text-3xl font-semibold mx-auto my-20">No Data</p>
-      }</div>
+      <div className="flex px-[31px] flex-wrap gap-6">
+        {filtered.length > 0 ? (
+          filtered.map((flt) => (
+            <Link key={flt._id} to={`/product-detail/${flt._id}`}>
+              <div
+                
+                className="bg-white cursor-pointer shadow-md hover:shadow-lg overflow-hidden transition-shadow rounded-lg w-[250px]"
+              >
+                <img
+                  src={flt.imageUrls[0]}
+                  alt="product image"
+                  className="h-[320px] sm:h-[180px] w-full object-cover hover:scale-105 transition-scale duration-300"
+                />
+                <div className="p-3 flex flex-col gap-2 w-full">
+                  <p className="text-lg font-semibold text-slate-700 truncate">
+                    {flt.name}
+                  </p>
+                  <div className="flex items-center gap-1">
+                    <p className="text-sm text-gray-600 truncate w-full">
+                      Rp{" "}
+                      {(+flt.regularPrice - +flt.discountPrice).toLocaleString(
+                        "en-US"
+                      )}
+                    </p>
+                  </div>
+                </div>
+              </div>{" "}
+            </Link>
+          ))
+        ) : (
+          <p className="text-3xl font-semibold mx-auto my-20">No Data</p>
+        )}
+      </div>
     </div>
   );
 };
